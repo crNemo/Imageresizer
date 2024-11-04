18e86036a9330e9dabcf1ac2ecdb008fc38dc0a4
Bibash Thapa <crnemo07@gmail.com>
2024-11-04 10:59:44 +0545

    Added resizing by new width and height.

--- imgresizer.py
+++ imgresizer.py
@@ -1,12 +1,12 @@
 import cv2
 
-imgsrc=input("Enter the name of image or source: ")
-new_imgsrc=input("Enter the new name of image or source with extension: ")
-scale=int(input("Enter by what percent you want to resize (In percentage): "))
-img=cv2.imread(f"{imgsrc}", cv2.IMREAD_UNCHANGED)
+imgsrc=input("Enter the source: ")
+new_imgsrc=input("Enter the new name of image with extension: ")
 
-new_width=int(img.shape[1]*scale/100)
-new_height=int(img.shape[0]*scale/100)
+new_width=int(input("Enter the new width: "))
+new_height=int(input("Enter the new new height: "))
+
+img=cv2.imread(f"{imgsrc}", cv2.IMREAD_UNCHANGED)
 
 new_img=cv2.resize(img,(new_width,new_height))
 cv2.imwrite(f"{new_imgsrc}", new_img)
